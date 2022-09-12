4b882c501b4dd8bb27a8fb3cbf80a8370b47c79d
crkn07 <113365562+crkn07@users.noreply.github.com>
2022-09-12 16:35:25 +0200

    Add files via upload

---
+++ ft_putstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crtorres <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/27 11:08:20 by crtorres          #+#    #+#             */
+/*   Updated: 2022/08/27 11:10:20 by crtorres         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+/*
+int  main(void)
+{
+	char str[] = "42 Madrid!";
+	char *pstr;
+	pstr = str;
+	ft_putstr(pstr);
+}
+*/
