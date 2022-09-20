099225f997624213044b4f960dbcd13806fdb688
crkn07 <113365562+crkn07@users.noreply.github.com>
2022-09-20 16:20:14 +0200

    Add files via upload

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: crtorres <crtorres@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 12:30:20 by crtorres          #+#    #+#             */
-/*   Updated: 2022/09/19 15:03:33 by crtorres         ###   ########.fr       */
+/*   Updated: 2022/09/20 15:48:44 by crtorres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,19 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-	new_string = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	new_string = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 2));
 	if (new_string == NULL)
 		return (NULL);
 	while (s1[i])
 	{
-		new_string[j++] = s1[i++];
+		new_string[i] = s1[i];
+		i++;
 	}
-	while (s2[i])
+	while (s2[j])
 	{
-		new_string[j++] = s2[i++];
+		new_string[i + j] = s2[j];
+		j++;
 	}
-	new_string[j] = '\0';
+	new_string[i + j] = '\0';
 	return (new_string);
 }
